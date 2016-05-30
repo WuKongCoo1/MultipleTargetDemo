45a9fce5e9b71a1af3fef25ac6ed90098b6a2c1b
WuKongCoo1 <wukong@wukedemac-mini.local>
2016-05-30 14:46:05 +0800

    Initial Commit

---
+++ MultipleTargetDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultipleTargetDemo
+//
+//  Created by 吴珂 on 16/5/30.
+//  Copyright © 2016年 吴珂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultipleTargetDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MultipleTargetDemo
+//
+//  Created by 吴珂 on 16/5/30.
+//  Copyright © 2016年 吴珂. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
